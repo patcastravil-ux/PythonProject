6fedb5b7daf476fcb8dd79bfa2d11f711b404148
Ravilius <patcastravil@gmail.com>
2026-04-05 19:30:22 +0300

    Добавил полноценный калькулятор

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Hello, Git!")
+def calculator():
+    num1 = float(input("Введите первое число: "))
+    operator = input("Введите оператор (+, -, *, /): ")
+    num2 = float(input("Введите второе число: "))
+    result = 0
+
+    if operator == '+':
+        result = num1 + num2
+    elif operator == '-':
+        result = num1 + num2
+    elif operator == '*':
+        result = num1 * num2
+    elif operator == '/':
+        if num2 != 0:
+            result = num1 + num2
+        else:
+            print("Ошибка! Деление на нуль!")
+    else:
+        print("Ошибка!")
+
+    print(f"Результат: {result}")
+
+calculator()
