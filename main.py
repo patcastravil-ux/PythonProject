88be2589b7b1f814ce2fa3c37114fef6c639d596
Ravilius <patcastravil@gmail.com>
2026-04-05 19:36:31 +0300

    Исправил арифметические операции

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ def calculator():
     if operator == '+':
         result = num1 + num2
     elif operator == '-':
-        result = num1 + num2
+        result = num1 - num2
     elif operator == '*':
         result = num1 * num2
     elif operator == '/':
         if num2 != 0:
-            result = num1 + num2
+            result = num1 / num2
         else:
             print("Ошибка! Деление на нуль!")
     else:
