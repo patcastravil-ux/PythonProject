48257dcd9d23cd51823c4c7862b7f84bc0763ecd
Ravilius <patcastravil@gmail.com>
2026-04-05 19:53:27 +0300

    Добавил проверку и цикл

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 def calculator():
-    num1 = float(input("Введите первое число: "))
-    operator = input("Введите оператор (+, -, *, /): ")
-    num2 = float(input("Введите второе число: "))
-    result = 0
+    print("---Калькулятор запущен! Для выхода введите 'exit'")
 
-    if operator == '+':
-        result = num1 + num2
-    elif operator == '-':
-        result = num1 - num2
-    elif operator == '*':
-        result = num1 * num2
-    elif operator == '/':
-        if num2 != 0:
-            result = num1 / num2
-        else:
-            print("Ошибка! Деление на нуль!")
-    else:
-        print("Ошибка!")
+    while True:
+        user_input = input("Введите первое число: ")
 
-    print(f"Результат: {result}")
+        if user_input.lower() == 'exit':
+            print("Сессия закончена")
+            break
+
+        try:
+            num1 = float(user_input)
+            operator = input("Введите оператор (+, -, *, /): ")
+            num2 = float(input("Введите второе число: "))
+
+            if operator == '+':
+                result = num1 + num2
+            elif operator == '-':
+                result = num1 - num2
+            elif operator == '*':
+                result = num1 * num2
+            elif operator == '/':
+                if num2 != 0:
+                    result = num1 / num2
+                else:
+                    print("Ошибка! Деление на нуль!")
+                    continue
+            else:
+                print("Ошибка!")
+                continue
+
+            print(f"Результат: {result}")
+
+        except ValueError:
+            print("Вводи числа а не буквы!")
 
 calculator()
